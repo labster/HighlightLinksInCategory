3820a7f8fd15ac17feb5de9cbbb51a7009607a40
Brent Laabs <bslaabs@gmail.com>
2014-01-16 18:54:14 -0800

    add the code for the first working extension version

---
+++ HighlightLinksInCategory.php
@@ -0,0 +1,62 @@
+<?php
+# ATT Category List
+# Copyright 2013, Brent Laabs
+# Released under a MIT-style license, as well as under the same terms
+#    as Mediawiki.  See LICENSE for details
+
+
+$wgExtensionCredits['other'][] = array( 
+        'name' => 'Highlight Link in Category', 
+        'author' => 'Brent Laabs',
+        'version' => '0.5',
+        'url' => 'https://github.com/labster/mediawiki-highlight-links-in-category',
+        'descriptionmsg' => 'highlight-link-in-category',
+); 
+
+$wgHooks['LinkEnd'][] = 'HighlightCategoryLinks::HCLExtensionLinkEnd';
+
+
+class HighlightCategoryLinks {
+
+	private static $CategoryTrope;
+	private static $CategoryYMMV;
+
+	public static function HCLExtensionLinkEnd( $dummy, Title $target, array $options, &$html, array &$attribs, &$ret ) {
+		if (isset($attribs['class'])) {
+			return true; # don't mess with it if we have interwiki/broken/redirect
+		} elseif ( self::pageInCategory("YMMV",  self::$CategoryYMMV,  $target) ) {
+			$attribs['class'] = "ymmvlink";
+		} elseif ( self::pageInCategory("Trope", self::$CategoryTrope, $target) ) {
+			$attribs['class'] = "tropelink";
+		}
+		return true;
+	}
+
+	private static function pageInCategory ($category, &$categoryArray, $target) {
+		if (! $categoryArray) {
+			self::getCatHash($category, $categoryArray);
+		}
+		return isset($categoryArray[$target->getArticleID()]);
+	}
+
+	private static function getCatHash ($category, &$categoryArray) {
+		# Check memcached first
+		$categoryArray = $wgMemc->get("ATTCategoryList:$category");
+		if ($categoryArray) { return; }
+
+		# We need to look this up in the DB
+		$underscored_category = $category;
+		preg_replace( '/ /', '_', $underscored_category);
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select( 'categorylinks', 'cl_from', array( 'cl_to' => $underscored_category ) );
+		foreach ( $res as $row ) {
+			$categoryArray[$row->cl_from] = True;  # map page ids to true for O(1) lookups
+		}
+
+		# Cache result for a day, and return
+		$wgMemc->set("ATTCategoryList:$category", $categoryArray, 86400 );
+		return;
+	}
+
+}
+
