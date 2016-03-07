a6b0734a4e869d10a70e73fce01b2b016da8ea3e
Brent Laabs <blaabs@perceptyx.com>
2016-03-06 21:26:01 -0800

    Rewrite using GetLinkColours hook.
    The old version slurped a bunch of data and wasted a lot of cache
    to store that.  This new version just asks the DB for categories
    once per request by using the hook that lets us set all the internal
    link colors at once.  Should be faster, even without caching.

--- HighlightLinksInCategory.php
+++ HighlightLinksInCategory.php
@@ -1,73 +1,69 @@
 <?php
 # Extension: Highlight Links in Category
-# Copyright 2013, Brent Laabs and GethN7
+# Copyright 2013, 2016, Brent Laabs and GethN7
 # Released under a MIT-style license, as well as under the same terms
 #    as Mediawiki.  See LICENSE for details
 
 
-$wgExtensionCredits['other'][] = array( 
-        'name' => 'Highlight Link in Category', 
+$wgExtensionCredits['other'][] = array(
+        'name' => 'Highlight Links in Category',
         'author' => 'Brent Laabs, Arcane 21 (GethN7)',
-        'version' => '0.5',
+        'version' => '0.9',
         'url' => 'https://github.com/labster/mediawiki-highlight-links-in-category',
         'descriptionmsg' => 'Highlights links in categories via customizable CSS classes',
-); 
+);
 
-$wgHooks['LinkEnd'][] = 'HighlightCategoryLinks::HCLExtensionLinkEnd';
+$wgHooks['GetLinkColours'][] = 'HighlightLinksInCategory::onGetLinkColours';
 
 
-class HighlightCategoryLinks {
+# Configuration:
+# $wgHighlightCategories is an array that configures which categories
+# get an added CSS class.
+# The keys are the Category names -- with underscores, without "Category:"
+# The values are the CSS classes to add to links of that category
+# If you want to make it so that the behavior of one category overrides
+# the other, use CSS, like '.class1.class2 { only class2 stuff }'
 
-	private static $CategoryTrope;
-	private static $CategoryYMMVTrope;
 
-	public static function HCLExtensionLinkEnd( $dummy, Title $target, array $options, &$html, array &$attribs, &$ret ) {
-		$newclass = '';
-		if (isset($attribs['class'])) {
-			if ($attribs['class'] == "mw-changeslist-title") {
-				$newclass = "mw-changeslist-title ";
-			}
-			else {
-				return true; # don't mess with it if we have interwiki/broken/redirect
-			}
-		}
+$wgHighlightCategories = array(
+    "Trope" => 'trope',
+    "YMMV_Trope" => 'ymmv',
+);
 
-		if ( self::pageInCategory( "YMMV_Trope", self::$CategoryYMMVTrope, $target) ) {
-			$attribs['class'] = $newclass . "ymmvlink";
-		} elseif ( self::pageInCategory("Trope", self::$CategoryTrope, $target) ) {
-			$attribs['class'] = $newclass . "tropelink";
-		}
+class HighlightLinksInCategory {
 
-		return true;
-	}
+    public static function onGetLinkColours( $linkcolour_ids, &$colours ) {
 
-	private static function pageInCategory ($category, &$categoryArray, $target) {
-		if (! $categoryArray) {
-			self::getCatHash($category, $categoryArray);
-		}
-		return isset($categoryArray[$target->getArticleID()]);
-	}
+        if ( ! count($wgHighlightCategories) ) {
+            return true;
+        }
 
-	private static function getCatHash ($category, &$categoryArray) {
-		global $wgMemc;
+        # linkcolour_ids only contains pages that exist, which does a lot
+        # of our work for us
+        $pageIDs  = array_keys($linkcolour_ids);
+        $catNames = array_keys($wgHighlightCategories);
 
-		#preg_replace( '/ /', '_', $category);
+        # Get page ids with appropriate categories from the DB
+        # There's an index on (cl_from, cl_to) so this should be fast
+        $dbr = wfGetDB( DB_SLAVE );
+        $res = $dbr->select( 'categorylinks',
+            array('cl_from', 'cl_to'),
+            $dbr->makeList( array(
+                $dbr->makeList(
+                    array( 'cl_from' => $pageIDs ), LIST_OR),
+                $dbr->makeList(
+                    array( 'cl_to'   => $catNames), LIST_OR)
+                ),
+                LIST_AND
+            )
+        );
 
-		# Check memcached first (can be commented out if the absence of memcached)
-		$categoryArray = $wgMemc->get("HLCategoryList:$category");
-		if ($categoryArray) { return; }
-
-		# We need to look this up in the DB
-		$dbr = wfGetDB( DB_SLAVE );
-		$res = $dbr->select( 'categorylinks', 'cl_from', array( 'cl_to' => $category ) );
-		foreach ( $res as $row ) {
-			$categoryArray[$row->cl_from] = True;  # map page ids to true for O(1) lookups
-		}
-
-		# Cache result for a day, and return (can be commented out if the absence of memcached)
-		$wgMemc->set("HLCategoryList:$category", $categoryArray, 86400 );
-		return;
-	}
+        # Add the color classes to each page
+        foreach ( $res as $s ) {
+            $colours[ $linkcolour_ids[ $s->cl_from ] ]
+                .= ' ' . $wgHighlightCategories[ $s->cl_to ];
+        }
+    }
 
 }
 
