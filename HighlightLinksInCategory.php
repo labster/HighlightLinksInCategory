c2057e3dd0db993550a564321c870747a0f889f2
labster <bslaabs@gmail.com>
2016-03-19 18:29:44 -0700

    Add extension.json, expand README

--- HighlightLinksInCategory.php
+++ HighlightLinksInCategory.php
@@ -1,47 +1,33 @@
 <?php
 # Extension: Highlight Links in Category
-# Copyright 2013, 2016, Brent Laabs and GethN7
-# Released under a MIT-style license, as well as under the same terms
-#    as Mediawiki.  See LICENSE for details
+# Copyright 2013, 2016 Brent Laabs
+# Released under a MIT-style license.  See LICENSE for details
 
-
-$wgExtensionCredits['other'][] = array(
-        'name' => 'Highlight Links in Category',
-        'author' => 'Brent Laabs, Arcane 21 (GethN7)',
-        'version' => '0.9',
-        'url' => 'https://github.com/labster/mediawiki-highlight-links-in-category',
-        'descriptionmsg' => 'Highlights links in categories via customizable CSS classes',
-);
-
-$wgHooks['GetLinkColours'][] = 'HighlightLinksInCategory::onGetLinkColours';
-
-
-# Configuration:
-# $wgHighlightCategories is an array that configures which categories
-# get an added CSS class.
-# The keys are the Category names -- with underscores, without "Category:"
-# The values are the CSS classes to add to links of that category
-# If you want to make it so that the behavior of one category overrides
-# the other, use CSS, like '.class1.class2 { only class2 stuff }'
-
-
-$wgHighlightCategories = array(
-    "Trope" => 'trope',
-    "YMMV_Trope" => 'ymmv',
-);
+# You can probably uncomment this to get it to work under 1.12
+#
+#$wgExtensionCredits['other'][] = array(
+#        'name' => 'Highlight Links in Category',
+#        'author' => 'Brent Laabs',
+#        'version' => '0.9',
+#        'url' => 'https://github.com/labster/mediawiki-highlight-links-in-category',
+#        'descriptionmsg' => 'Highlights links in categories via customizable CSS classes',
+#);
+#
+# $wgHooks['GetLinkColours'][] = 'HighlightLinksInCategory::onGetLinkColours';
 
 class HighlightLinksInCategory {
 
     public static function onGetLinkColours( $linkcolour_ids, &$colours ) {
+	global $wgHighlightLinksInCategory;
 
-        if ( ! count($wgHighlightCategories) ) {
+        if ( ! count($wgHighlightLinksInCategory) ) {
             return true;
         }
 
         # linkcolour_ids only contains pages that exist, which does a lot
         # of our work for us
         $pageIDs  = array_keys($linkcolour_ids);
-        $catNames = array_keys($wgHighlightCategories);
+        $catNames = array_keys($wgHighlightLinksInCategory);
 
         # Get page ids with appropriate categories from the DB
         # There's an index on (cl_from, cl_to) so this should be fast
@@ -61,7 +47,7 @@ class HighlightLinksInCategory {
         # Add the color classes to each page
         foreach ( $res as $s ) {
             $colours[ $linkcolour_ids[ $s->cl_from ] ]
-                .= ' ' . $wgHighlightCategories[ $s->cl_to ];
+                .= ' ' . $wgHighlightLinksInCategory[ $s->cl_to ];
         }
     }
 
--- README.md
+++ README.md
@@ -1,6 +1,82 @@
-mediawiki-highlight-links-in-category
-=====================================
+Highlight Links in Category
+===========================
 
-Exactly what it says on the tin -- if a link is a member of a category, it gets a custom CSS class
+A Mediawiki Extension: If a link is a member of a category, it gets a custom CSS class.
 
-The CSS class can be modified via adding the CSS preferences to the MediaWiki:Common.css page.
+You can add as many of these as you want, but from a UX perspective it's probably
+not awesome to add 500 different style links.
+
+Requirements
+------------
+
+Mediawiki 1.25 probably, but only tested on 1.26 and above.  Theoretically can
+work on 1.12 but might require some changes.
+
+Installation
+------------
+
+Make sure that this directory is installed in `mediawiki/extensions` (or wherever
+your custom extension directory is.
+
+Then, add this to `LocalSettings.php`:
+
+		wfLoadExtension( 'HighlightLinksInCategory' );
+		$wgHighlightLinksInCategory = array(
+		    "Disambiguation_pages" => 'disambig',
+		    "Templates" => 'templates',
+		);
+
+Configuration
+-------------
+
+The global variable `$wgHighlightLinksInCategory` is an array that configures which
+categories get an added CSS class. The keys are the Category names, which must
+include underscores instead of spaces. Do not include the `Category:` namespace.
+The value for each key is the CSS class you would like to add to that
+category. If you want add more than one class, separate the class names with
+spaces, like:
+
+	$wgHighlightLinksInCategory = array( "My_Cat" => "class1 class2 class3" );
+
+If `$wgHighlightLinksInCategory` is not set or empty, this extension will do nothing.
+
+Styling
+-------
+
+Simply add the styles you need for these links to `Mediawiki:Common.css` on your wiki.
+For example:
+
+		a.disambig {
+			color: rebeccapurple
+		}
+		a.templates {
+			font-weight: bold;
+			background-color: #efe;
+		}
+
+If you want one category to override another, you'll have to specify it with CSS.
+
+		a.templates.disambig {
+			font-weight: normal;
+			background-color: transparent;
+			color: rebeccapurple;
+		}
+
+Or alternatively, you could have defined the original `a.templates` rule as
+`a.templates:not(.disambig)` instead.
+
+There's also the link icon approach:
+
+		a.superlink {
+			background-image: url(super.png);
+			padding-left: 16px;
+		}
+
+License
+-------
+
+MIT. See LICENSE for details.
+
+Have the appropriate amount of fun!
+
+
